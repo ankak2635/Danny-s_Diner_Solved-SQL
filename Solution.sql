1539327e5d488e3e5007886cd2f790a73a97ca3d
Ankit Kumar <ank.ak2635@gmail.com>
2023-05-19 09:52:21 -0400

    Adds solution 10

--- Solution.sql
+++ Solution.sql
@@ -23,3 +23,31 @@ ON sales.product_id = points_cte.product_id
 GROUP BY sales.customer_id
 ORDER BY sales.customer_id;
 
+--10. In the first week after a customer joins the program (including their join date) they earn 2x points on all items, not just sushi how many points do customer A and B have at the end of January?
+-- cte to implement date restrictions 
+WITH date_cte as(
+SELECT *, 
+DATE (join_date + INTERVAL '6 DAYS') as valid_date,
+DATE ('2021-01-31') as end_date    
+FROM members
+),
+
+-- another cte to assign points
+points_cte as(
+SELECT date_cte.customer_id, 
+CASE
+	WHEN sales.order_date BETWEEN date_cte.join_date AND date_cte.valid_date THEN menu.price *20
+    WHEN menu.product_id = 1 then menu.price* 20
+    ELSE menu.price*10 END
+    AS points
+ FROM date_cte
+ JOIN sales
+ ON sales.customer_id = date_cte.customer_id
+ JOIN menu
+ ON sales.product_id = menu.product_id
+ WHERE sales.order_date <= date_cte.end_date
+ )
+  
+ SELECT customer_id, sum(points) AS Points_till_Jan_end
+ FROM points_cte
+ GROUP BY customer_id
